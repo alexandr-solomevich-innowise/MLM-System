2c6756030bc79d84ad82dfe6015a14491b956972
Alexandr Solomevich <alexandr.solomevich@gmail.com>
2022-07-22 09:45:13 +0300

    Create basics

---
+++ contracts/Functionality.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Functionality {
+    address public systemAccount = 0x43169f86f1A575a8Fa7ceA12E1883E22A848f0F8;
+
+    mapping(address => User) public users;
+    mapping(address => mapping(address => bool)) directPartners;
+    //mapping(address => uint) userLevel;
+
+    struct User {
+        //address[] directPartners;
+        uint8 level;
+        bool active;
+        uint partnersCount;
+        //mapping(address => bool) dP;
+    }
+
+    modifier onlyNewUser(address _id) {
+        require(!users[msg.sender].active, "You've already entered the system.");
+        _;
+    }
+
+    function enterSystem() onlyNewUser(msg.sender) external {
+        users[msg.sender].active = true;
+    }
+
+    function enterSystem(address _partnerId) onlyNewUser(msg.sender) external {
+        require(users[_partnerId].active, "The user with such referral reference does not exist.");
+        users[msg.sender].active = true;
+        //users[_partnerId].directPartners.push(msg.sender);
+        directPartners[_partnerId][msg.sender] = true;
+        users[_partnerId].partnersCount++;
+    }
+
+    function viewLevel() external view returns(uint8) {
+        return users[msg.sender].level;
+    }
+
+    function viewNumberOfDirectPartners() external view returns(uint) {
+        return users[msg.sender].partnersCount;
+    }
+
+    function viewLevelOfDirectPartner(address _partner) external view returns (uint8) {
+        require(directPartners[msg.sender][_partner], "There is no such partner in your list.");
+        return users[_partner].level;
+    }
+}
