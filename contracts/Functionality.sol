3ccb0c21f89801edb432f07819ff21e15170405e
Alexandr Solomevich <alexandr.solomevich@gmail.com>
2022-07-22 21:20:18 +0300

    Add update level

--- contracts/Functionality.sol
+++ contracts/Functionality.sol
@@ -4,17 +4,33 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Functionality {
-    address payable systemAccount;// = 0x43169f86f1A575a8Fa7ceA12E1883E22A848f0F8;
+    address systemAccount = 0x43169f86f1A575a8Fa7ceA12E1883E22A848f0F8;
 
     mapping(address => User) public users;
     mapping(address => mapping(address => bool)) directPartners;
     //mapping(address => uint) userLevel;
+    mapping(uint => uint) public levelTable;
+
+    constructor() {
+        levelTable[1] = 5 * 10 ** 6;
+        levelTable[2] = 10 * 10 ** 6;
+        levelTable[3] = 20 * 10 ** 6;
+        levelTable[4] = 50 * 10 ** 6;
+        levelTable[5] = 100 * 10 ** 6;
+        levelTable[6] = 200 * 10 ** 6;
+        levelTable[7] = 500 * 10 ** 6;
+        levelTable[8] = 1000 * 10 ** 6;
+        levelTable[9] = 2000 * 10 ** 6;
+        levelTable[10] = 5000 * 10 ** 6;
+    }
 
     struct User {
         //address[] directPartners;
         uint8 level;
         bool active;
         uint partnersCount;
+        uint investmentAmount;
+        address referral; //owner==address(0);
         //mapping(address => bool) dP;
     }
 
@@ -23,27 +39,28 @@ contract Functionality {
         _;
     }
 
-    function enterSystem() onlyNewUser(msg.sender) external {
+    function enterSystem() onlyNewUser(msg.sender) public {
         users[msg.sender].active = true;
     }
 
-    function enterSystem(address _partnerId) onlyNewUser(msg.sender) external {
+    function enterSystem(address _partnerId) onlyNewUser(msg.sender) public {
         require(users[_partnerId].active, "The user with such referral reference does not exist.");
         users[msg.sender].active = true;
+        users[msg.sender].referral = _partnerId;
         //users[_partnerId].directPartners.push(msg.sender);
         directPartners[_partnerId][msg.sender] = true;
         users[_partnerId].partnersCount++;
     }
 
-    function viewLevel() external view returns(uint8) {
+    function viewLevel() public view returns(uint8) {
         return users[msg.sender].level;
     }
 
-    function viewDirectPartnersNumber() external view returns(uint) {
+    function viewDirectPartnersNumber() public view returns(uint) {
         return users[msg.sender].partnersCount;
     }
 
-    function viewDirectPartnerLevel(address _partner) external view returns (uint8) {
+    function viewDirectPartnerLevel(address _partner) public view returns (uint8) {
         require(directPartners[msg.sender][_partner], "There is no such partner in your list.");
         return users[_partner].level;
     }
@@ -51,7 +68,23 @@ contract Functionality {
     function invest() public payable {
         (bool success,) = systemAccount.call{value: msg.value}("");
         require(success, "Failed to send money");
+        updateLevel(msg.value, msg.sender);
+        //зачислить на контракт 5%
     }
 
+    function updateLevel(uint _value, address _user) private {
+        uint currentLevel = users[_user].level;
+        uint investment = users[_user].investmentAmount;
+
+        investment += _value;
+
+        uint nextLevel = currentLevel + 1;
+        if (levelTable[nextLevel] <= investment)
+            users[_user].level++;
+    }
+
+    function withdraw() public {
+
+    }
 
 }
